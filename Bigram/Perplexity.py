73e0b528a74fdd1f3c6e3e62f2f47f7c4c77558c
Pedro Pio <pedrobpio@gmail.com>
2024-11-13 10:00:17 -0300

    add readme and comments

--- Bigram/Perplexity.py
+++ Bigram/Perplexity.py
@@ -3,44 +3,50 @@ from BPE import BPETokenizer
 from tqdm.autonotebook import tqdm
 import math
 
+# this class is basicaly the same as the Bigram class, however, instead of predicting the next word, it computes the perplexity
 class Perplexity:
+
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
         self.vocab = tokenizer.vocab
-        # build add and and 
 
-        # build matrix of occurency
         self.vocab_size = len(self.vocab)
+        # matrix cotaining the quantities of each token, note that it starts with ones to aply smoothing 
         self.bigram_matrix = np.ones((self.vocab_size, self.vocab_size))
         self.prob_matrix = np.zeros((self.vocab_size, self.vocab_size))
+        # dictionary to optimize the enconding process
         self.encoded_dict = {}
 
-
+    # mwthod that update the bigram matrix
     def update_bigram_matrix(self, text: str):
+        # adds the init and end tokens to the text
         text = "<|s|> "+ text + " <|e|>"
+        # split the text into works
         splitted_text = text.split(" ")
         tokens = []
+        # for each word, checks if it was already encoded, if it was, repleca the word for its tokens,
+        # if it wasnt, aply the tokenizer.encode funcion
         for word in splitted_text:
             val = self.encoded_dict.get(word, self.tokenizer.encode(word))
             tokens += val
             self.encoded_dict[word] = val
-        # tokens = self.tokenizer.encode(text)
-        # print(tokens)
+        
+        # loop to update the bigram matrix
         for first_token, next_token in zip(tokens, tokens[1:]):
             self.bigram_matrix[first_token, next_token] +=1
 
 
-    
+    # method that set the probability matrix based on the bigram matrix
     def update_prob_matrix(self):
         self.prob_matrix = self.bigram_matrix / np.sum(self.bigram_matrix, axis=1, keepdims = True)
         
-
+    # method to train the 
     def train(self, list_of_texts):
         for text in tqdm(list_of_texts):
             self.update_bigram_matrix(text)
             self.update_prob_matrix()
 
-    
+    # method to compute perplexity
     def compute_perplexity(self, text: str):
         tokens = self.tokenizer.encode(text)
         perplexity =1
@@ -50,15 +56,4 @@ class Perplexity:
             count +=1
         
         return math.pow(perplexity, -1/count)
-        # tokens_list = [5004]
-        
-    # def compute_perplexity(self, num_tokens=20):
-    #     tokens_list = [5004]
-    #     for i in range(num_tokens):
 
-
-    #         rng = np.random.default_rng()
-    #         next_token = rng.multinomial(1, self.prob_matrix[tokens_list[-1]], size=1).argmax(axis=-1)[0]
-    #         tokens_list.append(next_token)
-        
-    #     return self.tokenizer.decode(tokens_list)
---
+++ Bigram/README.md
@@ -0,0 +1,10 @@
+## Bigram
+
+This code was inspired on Andrej Karpathy's [youtube video](https://www.youtube.com/watch?v=PaCmpygFfXo)
+
+The code of Bigram can be found at [Bigram.py](https://github.com/pedrobpio/nlp/blob/main/Bigram/Bigram.py)
+
+The code of Perplexity can be found at [Perplexity.py](https://github.com/pedrobpio/nlp/blob/main/Bigram/Perplexity.py)
+
+Here you will also find a notebook file [Perplexity.py](https://github.com/pedrobpio/nlp/blob/main/Bigram/Perplexity.py) that shows how to train the bigram and how to compute the perplexity.
+
